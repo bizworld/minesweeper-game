c332bafb67ccd49c6f0600389dd57223ffb2eeb5
bizworld <bizworld@outlook.com>
2017-10-21 15:55:15 -0400

    Print the simulated game board..

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,5 +1,5 @@
-/* Hard code or manually create an empty game board and save to a constant
-variable, blankLine */
+/* Hard code or manually create an empty game board (i.e. an empty board row)
+and save to a constant variable, blankLine */
 const blankLine = ' | | ';
 
 /* Print the empty game board by using console.log to print a message and
@@ -18,3 +18,11 @@ const guessLine = '1 | | ';
 bombLine represents what a board row will look like when a player clicks
 and reveals a bomb. */
 const bombLine = ' | B | ';
+
+/* Print what a board might look like during a game by first using console.log()
+to log a message. Then use console.log() to log or print guessLine, bombLine,
+and blankLine, respectively. */
+console.log("This is what a board with a guess and a bomb on it would look like:");
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
