bcfe998785213ef93c0e7151c77015b83cfe7b9f
bizworld <bizworld@outlook.com>
2017-10-21 12:20:50 -0400

    Print a message with an empty game board.

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,12 +1,10 @@
 /* Hard code or manually create an empty game board and save to a constant
-variable, blankLine
-*/
+variable, blankLine */
 const blankLine = ' | | ';
 
-// Use console.log to print a message
+/* Print the empty game board by using console.log to print a message and
+to log or print the blankLine constant three times */
 console.log("This is what an empty board would look like: ");
-
-// Use console.log to log or print the blankLine constant three times
 console.log(blankLine);
 console.log(blankLine);
 console.log(blankLine);
