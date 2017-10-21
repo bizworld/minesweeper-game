f05b7c73da12279e5f22705796cac96656202247
bizworld <bizworld@outlook.com>
2017-10-21 15:28:24 -0400

    Hard code (manually create) a simulated game board.

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -8,3 +8,13 @@ console.log("This is what an empty board would look like: ");
 console.log(blankLine);
 console.log(blankLine);
 console.log(blankLine);
+
+/* Create a constant variable, guessLine, to hold a hard-coded row.
+guessLine represents what a board row will look like when a player guesses
+by "clicking" (selecting) the first square of this row. */
+const guessLine = '1 | | ';
+
+/* Create a constant variable, bombLine, to hold a hard-coded row.
+bombLine represents what a board row will look like when a player clicks
+and reveals a bomb. */
+const bombLine = ' | B | ';
