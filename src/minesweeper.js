63a5f6152e599743e26a678fd5d633ec102bb90d
bizworld <bizworld@outlook.com>
2017-10-24 21:52:57 -0400

    Create the structure for a game board.

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,28 +1,12 @@
-/* Hard code or manually create an empty game board (i.e. an empty board row)
-and save to a constant variable, blankLine */
-const blankLine = ' | | ';
+/* Create a generic game board that can be used for any purpose in the future,
+using a constant variable, board (that's mutable but not reassignable).
+Use the board variable to store the same 3 x 3 grid structure as the previous
+manually generated board. */
+const board = [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+  [' ', ' ', ' ']
+];
 
-/* Print the empty game board by using console.log to print a message and
-to log or print the blankLine constant three times */
-console.log("This is what an empty board would look like: ");
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
-
-/* Create a constant variable, guessLine, to hold a hard-coded row.
-guessLine represents what a board row will look like when a player guesses
-by "clicking" (selecting) the first square of this row. */
-const guessLine = '1| | ';
-
-/* Create a constant variable, bombLine, to hold a hard-coded row.
-bombLine represents what a board row will look like when a player clicks
-and reveals a bomb. */
-const bombLine = ' |B| ';
-
-/* Print what a board might look like during a game by first using console.log()
-to log a message. Then use console.log() to log or print guessLine, bombLine,
-and blankLine, respectively. */
-console.log("This is what a board with a guess and a bomb on it would look like:");
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+// Log board, using console.log
+console.log(board);
