9530533548818f8357c4981ae865da873850f4f1
bizworld <bizworld@outlook.com>
2017-10-27 22:57:28 -0400

    Generate a bomb board dynamically.

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,5 +1,6 @@
 /* Create a constant variable, generatePlayerBoard, to store a function that will
-generate a blank board of a given size to hold the player's guesses. */
+generate a blank board of a given size to hold the player's guesses.
+This function will dynamically generate a player's board no matter the size. */
 const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
   // board represents the overall game board.
   const board = [];
@@ -21,6 +22,25 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
   return board;
 };
 
-/* testing the generatePlayerBoard() function with a few diff sets of arguments
-in console.log (delete these calls, once done). */
-console.log(generatePlayerBoard(3,3));
+/* Create a constant variable, generateBombBoard, to hold a function that will
+dynamically generate a bomb board. */
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  // board represents the overall bomb board.
+  const board = [];
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    // row represents a single row to be added to the game board.
+    const row = [];
+    // for loop that loops through the number of columns specified to the function
+    for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++){
+      /* Add null to each column in the row array, which should match
+      the number of columns passed to the function. The null represents absence
+      of values on the squares on the board. This code will add the correct
+      number of null to each row. */
+      row.push(null);
+    }
+    // push the newly-created row into the board array
+    board.push(row);
+  }
+  // return the bomb board, board
+  return board;
+};
