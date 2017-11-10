28e9fc021c8701db3be55d953ed981131f2eae23
bizworld <bizworld@outlook.com>
2017-11-09 21:37:58 -0500

    Add generatePlayerBoard() function to the Board class and update it to a method.

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -128,6 +128,32 @@ class Board {
     console.log(this._playerBoard.map(row => row.join(' | ')).join('\n'));
   }
 
+  /* Create a static method .generatePlayerBoard(), that will generate a blank
+  board of a given size to hold the player's guesses. This function will
+  dynamically generate a player's board no matter the size. (4 line 10) */
+  static generatePlayerBoard(numberOfRows, numberOfColumns) {
+    // board represents the overall game board.
+    const board = [];
+    for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+      // row represents a single row to be added to the game board.
+      const row = [];
+      // for loop that loops through the number of columns specified to the function
+      for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++){
+        /* Add empty spaces ' ' to each column in the row array, which should match
+        the number of columns passed to the function. The empty spaces represent
+        squares on the board. This code will add the correct number of
+        empty spaces (' ', or squares) to each row.*/
+        row.push(' ');
+      }
+      // push the newly-created row into the board array
+      board.push(row);
+    }
+    // return the game board, board
+    return board;
+  };
+
+
+
 
 
 }
@@ -138,29 +164,7 @@ class Board {
 
 
 
-/* Create a constant variable, generatePlayerBoard, to store a function that will
-generate a blank board of a given size to hold the player's guesses.
-This function will dynamically generate a player's board no matter the size. */
-const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
-  // board represents the overall game board.
-  const board = [];
-  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
-    // row represents a single row to be added to the game board.
-    const row = [];
-    // for loop that loops through the number of columns specified to the function
-    for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++){
-      /* Add empty spaces ' ' to each column in the row array, which should match
-      the number of columns passed to the function. The empty spaces represent
-      squares on the board. This code will add the correct number of
-      empty spaces (' ', or squares) to each row.*/
-      row.push(' ');
-    }
-    // push the newly-created row into the board array
-    board.push(row);
-  }
-  // return the game board, board
-  return board;
-};
+
 
 /* Create a constant variable, generateBombBoard, to hold a function that will
 dynamically generate a bomb board. */
